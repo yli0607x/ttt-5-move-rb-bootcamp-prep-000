00ac1d66b97fee02dd962f662ae57facd1633591
Yu Li <yli0607x@gmail.com>
2018-08-21 20:35:23 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,6 +11,6 @@ def input_to_index(user_input)
   user_input.to_i-1
 end
 
-def move(board, index, character="X")
-  board[index] = character
+def move(board, index, current_player="X")
+  board[index] = current_player
 end
